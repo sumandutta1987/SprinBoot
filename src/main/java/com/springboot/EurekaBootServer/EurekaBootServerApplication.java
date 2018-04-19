5d8b13ad5af399553ba021b8bd38dda7ee722072
Suman <Suman@Suman-Pc>
2018-04-19 18:32:16 -0500

    checking

--- src/main/java/com/springboot/EurekaBootServer/EurekaBootServerApplication.java
+++ src/main/java/com/springboot/EurekaBootServer/EurekaBootServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaBootServerApplication {
-	
+//main	
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaBootServerApplication.class, args);
 	}
